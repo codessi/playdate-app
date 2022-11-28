074a7ee133b5a62294adf9947c443670d57715d2
codessi <johansuh@gmail.com>
2022-11-28 13:45:43 -0800

    bottom menu nudged up

--- src/routes/Navigation.jsx
+++ src/routes/Navigation.jsx
@@ -7,7 +7,7 @@ const Navigation = () => {
                 <div className="text-center absolute border inset-x-0 top-0 text-gray-800 font-semibold  p-2 text-2xl">PLAY DATE</div>
                 <Outlet />
 
-                <div className="absolute bottom-0 border inset-x-0 text-center">
+                <div className="absolute bottom-14 border inset-x-0 text-center">
                     <ul className="flex justify-around">
                         <li className="border w-full p-3"><Link to='/'>ON/OFF</Link></li>
                         <li className="border w-full p-3"><Link to='/list'>LIST</Link></li>
