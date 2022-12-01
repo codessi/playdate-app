ebb6c65b909646336132b2d0f4f63769f84b659c
codessi <johansuh@gmail.com>
2022-12-01 10:05:16 -0800

    adding fixed

--- src/routes/Navigation.jsx
+++ src/routes/Navigation.jsx
@@ -3,11 +3,11 @@ import {Outlet, Link} from "react-router-dom"
 const Navigation = () => {
     return (
         <>
-            <div className="h-screen relative">
+            {/* <div className="h-screen relative"> */}
                 <div className="text-center fixed border inset-x-0 top-0 text-gray-800 font-semibold  p-2 text-2xl">PLAY DATE</div>
                 <Outlet />
 
-                <div className="absolute bottom-0 border inset-x-0 text-center">
+                <div className="fixed bottom-0 border inset-x-0 text-center">
                     <ul className="flex justify-around">
                         <li className="border w-full p-3"><Link to='/on-off'>ON/OFF</Link></li>
                         <li className="border w-full p-3"><Link to='/list'>LIST</Link></li>
@@ -15,7 +15,7 @@ const Navigation = () => {
                     </ul>
 
                 </div>
-            </div>
+            {/* </div> */}
             
         </>
         
